ab0eb717a992dff8ddb11945ce038db299da1677
markitus18 <marc18.garrigo@gmail.com>
2019-07-30 23:30:47 +0200

    Finished sprite cleanup, started player movement + shooting

---
+++ Super Pang Project/Assets/Project/Scripts/Hook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        boxCollider.size = new Vector2(boxCollider.size.x, spriteRenderer.sprite.bounds.size.y);
+        boxCollider.offset = new Vector2(boxCollider.offset.x, boxCollider.size.y / 2);
+    }
+}
---
+++ Super Pang Project/Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject hookPrefab;
+
+    public float walkSpeed = 1.0f;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    bool shooting = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bool run = false;
+
+        if (shooting == false)
+        {
+		    if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector3(-1.0f * walkSpeed, 0.0f, 0.0f));
+                spriteRenderer.flipX = true;
+                run = true;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector3(1.0f * walkSpeed, 0.0f, 0.0f));
+                spriteRenderer.flipX = false;
+                run = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                shooting = true;
+                animator.SetBool("shooting", true);
+                Instantiate(hookPrefab, new Vector3(transform.position.x, transform.position.y - 16, transform.position.z), Quaternion.identity);
+            }
+        }
+
+        if (animator.GetBool("running") != run)
+            animator.SetBool("running", run);
+    }
+
+    public void OnShotFinished()
+    {
+        shooting = false;
+        animator.SetBool("shooting", false);
+    }
+}
